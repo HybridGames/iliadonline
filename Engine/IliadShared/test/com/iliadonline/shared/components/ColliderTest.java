b97211a1e413014560764db210d9a73998828ff9
Christopher Lowenthal <clowenthal@hybridgames.net>
2013-08-31 19:42:03 -0400

    Added new Source Folders for tests
    All projects now have a "test" source folder for JUnit tests.

---
+++ Engine/IliadShared/test/com/iliadonline/shared/components/ColliderTest.java
@@ -0,0 +1,22 @@
+package com.iliadonline.shared.components;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ColliderTest
+{
+
+	@Test
+	public void testColliderColliderTypeFloatArray()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testColliderCollider()
+	{
+		fail("Not yet implemented");
+	}
+
+}
