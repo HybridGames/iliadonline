cd7009aa6eafd9c055b0b42a08d0e5b157900720
Christopher Lowenthal <clowenthal@hybridgames.net>
2013-09-02 11:52:58 -0400

    DB Connection Established
    We can connect to a file based DB for the local server. Checked and it
    creates the files we need in the Data Directory on the Ouya.
    Created the DataInterface to pull together the other interfaces. Still
    not 100% on how I want to handle the expect large number of interface
    methods for data access.

---
+++ Engine/IliadServer/src/com/iliadonline/server/data/DataInterface.java
@@ -0,0 +1,9 @@
+package com.iliadonline.server.data;
+
+/**
+ * A Gathering Interface
+ */
+public interface DataInterface extends GameObjectDataInterface, MapDataInterface
+{
+
+}
--- Engine/IliadServer/src/com/iliadonline/server/data/HsqlDataProvider.java
+++ Engine/IliadServer/src/com/iliadonline/server/data/HsqlDataProvider.java
@@ -1,13 +1,59 @@
 package com.iliadonline.server.data;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
+import com.badlogic.gdx.Gdx;
 import com.iliadonline.server.objects.GameObject;
 import com.iliadonline.server.objects.IliadMap;
 
-public class HsqlDataProvider implements GameObjectDataInterface, MapDataInterface
+public class HsqlDataProvider implements DataInterface
 {
+	private static final String tag = "com.iliadonline.server.data.HsqlDataProvider";
 	
+	protected Connection connection;
+	
+	/**
+	 * Creates a persistent database at the folder provided
+	 * @param location
+	 */
+	public HsqlDataProvider(File location)
+	{
+		 try {
+		     Class.forName("org.hsqldb.jdbc.JDBCDriver");
+		 } catch (Exception e) {
+		     System.err.println("ERROR: failed to load HSQLDB JDBC driver.");
+		     e.printStackTrace();
+		     return;
+		 }
+		
+		if(!location.isDirectory())
+		{
+			throw new IllegalArgumentException("Location must be a directory.");
+		}
+		
+		try
+		{
+			Gdx.app.log(tag, "DB Directory: " + location.getAbsolutePath());
+			this.connection = DriverManager.getConnection("jdbc:hsqldb:file:" + location.getAbsolutePath(), "SA", "");
+		}
+		catch (SQLException e)
+		{
+			//TODO: Log and Report DB Failure
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Creates an in memory database (non-persistent)
+	 */
+	public HsqlDataProvider()
+	{
+		
+	}
 
 	@Override
 	public List<IliadMap> loadMaps()
