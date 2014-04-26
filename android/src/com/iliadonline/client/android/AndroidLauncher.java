789c6583cd2d574590252addb9b7b28008bbe04a
Christopher Lowenthal <clowenhg@gmail.com>
2014-04-26 05:56:18 -0400

    Working on Ouya specific code. Ability to get User Information.

--- android/src/com/iliadonline/client/android/AndroidLauncher.java
+++ android/src/com/iliadonline/client/android/AndroidLauncher.java
@@ -1,5 +1,8 @@
 package com.iliadonline.client.android;
 
+import tv.ouya.console.api.CancelIgnoringOuyaResponseListener;
+import tv.ouya.console.api.GamerInfo;
+import tv.ouya.console.api.OuyaFacade;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -9,6 +12,25 @@ import com.iliadonline.client.IliadClient;
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
+		
+		CancelIgnoringOuyaResponseListener<GamerInfo> gamerInfoListener = new CancelIgnoringOuyaResponseListener<GamerInfo>()
+		{
+			@Override
+			public void onSuccess(GamerInfo result)
+			{
+				
+			}
+
+			@Override
+			public void onFailure(int errorCode, String errorMessage,
+					Bundle errorBundle)
+			{
+				
+			}
+		};
+		
+		OuyaFacade.getInstance().requestGamerInfo(gamerInfoListener);
+		
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new IliadClient(), config);
