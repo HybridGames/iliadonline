ec2e0d7007d5a5fd3b98581103322e19169a9cf3
Christopher Lowenthal <clowenhg@gmail.com>
2014-04-27 19:51:48 -0400

    Added Internet Permission

--- android/src/com/iliadonline/client/android/AndroidLauncher.java
+++ android/src/com/iliadonline/client/android/AndroidLauncher.java
@@ -1,22 +1,44 @@
 package com.iliadonline.client.android;
 
-import tv.ouya.console.api.CancelIgnoringOuyaResponseListener;
 import tv.ouya.console.api.GamerInfo;
 import tv.ouya.console.api.OuyaFacade;
+import tv.ouya.console.api.OuyaResponseListener;
 import android.os.Bundle;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.iliadonline.client.IliadClient;
 
-public class AndroidLauncher extends AndroidApplication
+public class AndroidLauncher extends AndroidApplication implements OuyaResponseListener<GamerInfo>
 {
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new IliadClient(), config);
+		
+		OuyaFacade.getInstance().requestGamerInfo(this);
+	}
+
+	@Override
+	public void onCancel()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFailure(int arg0, String arg1, Bundle arg2)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSuccess(GamerInfo gamerInfo)
+	{
+		Gdx.app.log("User Name", gamerInfo.getUsername());
 	}
 }
