1f4851fd198d347309b1edaa6a6426b192d05606
Christopher Lowenthal <clowenhg@gmail.com>
2014-07-15 21:15:24 -0400

    Trying to get Loading Screen to initiate a load from a text file.

--- core/src/com/iliadonline/client/splash/SplashScreen.java
+++ core/src/com/iliadonline/client/splash/SplashScreen.java
@@ -1,10 +1,19 @@
 package com.iliadonline.client.splash;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONTokener;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.iliadonline.client.IliadClient;
@@ -28,10 +37,7 @@ public class SplashScreen implements Screen
 	
 	@Override
 	public void render(float delta)
-	{
-		Gdx.app.log(tag, "Splash Render");
-		Gdx.app.log(tag, this.loading.toString());
-		
+	{		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 		
@@ -53,6 +59,33 @@ public class SplashScreen implements Screen
 		assetManager.load("gfx/splash/IliadLoading.png", Texture.class);
 		
 		while(!assetManager.update());
+		
+		FileHandle assetList = this.client.getConfig().getStaticAssetFolder().child("assets\\data\\assets.json");
+		
+		JSONTokener tokener;
+		try
+		{
+			tokener = new JSONTokener(new FileInputStream(assetList.file()));
+			
+			JSONArray assetArray = new JSONArray(tokener);
+			
+			for(int i = 0; i < assetArray.length(); i++)
+			{
+				assetManager.load(assetArray.getString(i), BitmapFont.class);
+			}
+		}
+		catch (JSONException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 		this.loading = assetManager.get("gfx/splash/IliadLoading.png");
 		sprite = new Sprite(this.loading);
